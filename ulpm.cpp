0ef2859c22e16a69aa89c817f56ccc678f830300
Jake Backer <jbacker42@gmail.com>
2016-05-28 20:45:55 +0000

    All kinds of good stuff

--- ulpm.cpp
+++ ulpm.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 string args[10];
 
+string packMan; // lol
+
 bool hasProgram (string program) {
     string command = "which " + program;
     if (system(command.c_str()) == 0) {
@@ -15,6 +17,7 @@ bool hasProgram (string program) {
 }
 
 //This can be condensed
+//Needs python and stuff like that
 string getPackageManager () {
     if (hasProgram("apt-get")) {
         return "apt-get";
@@ -37,25 +40,49 @@ string getPackageManager () {
     } else if (hasProgram("curl")) {
         return "curl";
     } else {
-        cout << "Cannot find package! Are you on Windows?" << endl;
+        cout << "Cannot find package manager! Are you on Windows?" << endl;
+        return "none";
     }
 }
 
 int main(int argc, char* argv[])
 {
+    string commands[argc];
+    
     for (int i=1; i<argc; i++) {
         args[i] = argv[i];
     }
     
-    cout << getPackageManager() << endl;
+    if (getPackageManager() == "none") {
+        return 1;
+    }
+    
+    packMan = getPackageManager();
 
     for (int i=1; i<argc; i++) {
         if (args[i][0] == '-') {
             cout << "Option: " << args[i] << endl;
+            commands[i] = "Option: " + args[i];
         } else if (args[i] == "install") {
             cout << "Install: " << args[i + 1] << endl;
+            commands[i] = "Install: " + args[i + 1];
+        } else if (args[i] == "remove") {
+            cout << "Remove: " << args[i + 1] << endl;
+            commands[i] = "Remove: " + args[i + 1];
+        } else if (args[i] == "upgrade") {
+            cout << "Upgrade" << endl;
+            commands[i] = "Upgrade";
+        } else if (args[i] == "update") {
+            cout << "Update: " << args[i + 1] << endl;
+            commands[i] = "Update: " + args[i + 1];
         }
     }
+    
+    for (int i=1; i < argc; i++) {
+        cout << commands[i] << endl;
+    }
 
+    
+    
    return 0;
 }
