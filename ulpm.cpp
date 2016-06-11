a631d4c1b62feb6bd4b80893805a0478942efc52
Jake Backer <jbacker42@gmail.com>
2016-06-11 23:09:44 +0000

    Change license
    Add license back
    
    Delete LICENSE.md
    
    Create LICENSE.md

--- ulpm.cpp
+++ ulpm.cpp
@@ -1,6 +1,23 @@
 /*
-    ULPM is a simple Unified Linux Package Manager with minimum requirements and works on all linux distributions.
-    Copyright (C) 2016  Jake Backer <jbacker42@gmail.com>
+    Copyright (C) 2016 Jake Richard Backer. All Rights Reserved.
+    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+
+    This code is free software; you may not modify the software although you may
+    redistribute it under the terms of the GNU General Public License version 2
+    only, as it is published by the Free Software Foundation.
+    
+    This code is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    version 2 for more details (a copy is included in the LICENSE file that
+    accompanied this code).
+    
+    You should have received a copy of the GNU General Public License version
+    2 along with this work; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+    
+    Please contact the author, Jake Backer, at <jbacker42@gmail.com> with any
+    questions.
 */
 
 #include <iostream>
